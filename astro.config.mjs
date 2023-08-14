29749ce7fc377096a57e6a465648bafc83f7bb18
Eashver Elango <eashvere@gmail.com>
2023-08-13 20:25:45 -0700

    added sitemap and description

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), sitemap()],
   vite: {
     build: {
       sourcemap: true
